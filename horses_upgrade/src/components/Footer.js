661ca08c970606db2b8a4d79177b636b273febcf
Pablo Acebedo <pablofelipe@me.com>
2021-11-11 07:50:40 -0500

    Almost ready

--- horses_upgrade/src/components/Footer.js
+++ horses_upgrade/src/components/Footer.js
@@ -21,14 +21,41 @@ function Footer(props) {
                </div>
                </section> 
                <div className="footer-links">
-                   <div className="footer-links-wrapper">
-                       <div className="footer-links-items">
-                           <h2>About Us</h2>
-                           <Link to='/sign-up'>How it works</Link>
-                           <Link to='/'>Testimonials</Link>
-                       </div>
-                   </div>
-               </div>
+               <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Contact Us</h2>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Destinations</Link>
+            <Link to='/'>Sponsorships</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>Videos</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>Ambassadors</Link>
+            <Link to='/'>Agency</Link>
+            <Link to='/'>Influencer</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Social Media</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
+        </div>
+               
+    </div>
                <section className='social-media'>
                    <div className="social-media-wrap">
                        <div className="footer-logo">
@@ -52,6 +79,30 @@ function Footer(props) {
                            >
                                <i className="fab fa-instagram"></i>
                            </Link>
+                           <Link
+              class='social-icon-link youtube'
+              to='/'
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <i class='fab fa-youtube' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <i class='fab fa-twitter' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <i class='fab fa-linkedin' />
+            </Link>
                        </div>
                    </div>
 
