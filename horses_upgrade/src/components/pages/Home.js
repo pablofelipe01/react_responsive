9a17406591fe1a2337f20b776e772ab0c0b54e30
Pablo Acebedo <pablofelipe@me.com>
2021-11-11 18:28:13 -0500

    m

---
+++ horses_upgrade/src/components/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../../App.css'
+import Cards from '../Cards';
+import Footer from '../Footer';
+import HorseSection from '../HorseSection'
+
+function Home () {
+    return (
+
+    <>
+        <HorseSection />
+        <Cards />
+        <Footer/>
+
+
+    
+    </>
+    )
+}
+
+export default Home;
